afefc8a8754fe1e6ce8ab447a55d476e5bec816d
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2021-01-27 04:53:07 +0330

    The license note is added to source codes

--- src/Db/Query/Oracle12c.php
+++ src/Db/Query/Oracle12c.php
@@ -1,5 +1,22 @@
-<?php namespace Pluf\Db\Query;
-
+<?php
+/*
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace Pluf\Db\Query;
 
 /**
  * Perform query operation on Oracle server.
@@ -9,6 +26,7 @@
  */
 class Oracle12c extends OracleAbstract
 {
+
     /**
      * Renders [limit].
      *
@@ -20,13 +38,10 @@ class Oracle12c extends OracleAbstract
             $cnt = (int) $this->args['limit']['cnt'];
             $shift = (int) $this->args['limit']['shift'];
 
-            return ' '.trim(
-                ($shift ? 'OFFSET '.$shift.' ROWS' : '').
-                ' '.
-                // as per spec 'NEXT' is synonymous to 'FIRST', so not bothering with it.
-                // https://docs.oracle.com/javadb/10.8.3.0/ref/rrefsqljoffsetfetch.html
-                ($cnt ? 'FETCH NEXT '.$cnt.' ROWS ONLY' : '')
-            );
+            return ' ' . trim(($shift ? 'OFFSET ' . $shift . ' ROWS' : '') . ' ' . 
+            // as per spec 'NEXT' is synonymous to 'FIRST', so not bothering with it.
+            // https://docs.oracle.com/javadb/10.8.3.0/ref/rrefsqljoffsetfetch.html
+            ($cnt ? 'FETCH NEXT ' . $cnt . ' ROWS ONLY' : ''));
         }
     }
 }
